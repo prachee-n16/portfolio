b264ef161d2a85ad0d14c49af70ad630d4db7b5b
PRACHEE NANDA <p4nanda@uwaterloo.ca>
2026-07-21 21:42:42 -0700

    init

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "prachee nanda",
+};
+
+export default function Home() {
+  return <main className="min-h-screen bg-white" />;
+}
